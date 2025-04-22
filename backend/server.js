c826027d9489191c5858d7d30ea14b199d5b09ad
sumitbudaniyaa <sumit.k.budaniya@gmail.com>
2025-04-22 10:16:14 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
